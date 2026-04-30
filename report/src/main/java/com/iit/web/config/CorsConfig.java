5f763ebc40072f859ba5e0203782be4e855b4e17
Vince Dizon <dizonvinceryan02@gmail.com>
2026-05-01 05:09:34 +0800

    Cors config

---
+++ report/src/main/java/com/iit/web/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.iit.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("respectful-victory-production-39bc.up.railway.app")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
